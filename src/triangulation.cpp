aec3366a773dd03465e468f5964540fcbb03afa7
zoumson <38358621+zoumson@users.noreply.github.com>
2021-06-13 02:11:16 +0800

    chinese to english triang

--- src/triangulation.cpp
+++ src/triangulation.cpp
@@ -28,7 +28,7 @@ void triangulation (
     vector<cv::Point3d>& points
 );
 
-// 像素坐标转相机归一化坐标
+// Pixel coordinates to camera normalized coordinates
 cv::Point2f pixel2cam( const cv::Point2d& p, const cv::Mat& K );
 
 int main ( int argc, char** argv )
@@ -55,17 +55,7 @@ int main ( int argc, char** argv )
         return -1;
     }
    
-    /*
-    if ( argc != 3 )
-    {
-        cout<<"usage: pose_estimation_2d2d img1 img2"<<endl;
-        return 1;
-    }
-    */
-    //-- 读取图像
- 
-    
-    //cout <<imagePath1 <<endl;
+    //-- read image 
     cv::Mat img_1 = cv::imread ( imagePath1, cv::IMREAD_COLOR );
     cv::Mat img_2 = cv::imread ( imagePath2, cv::IMREAD_COLOR );
 
@@ -73,20 +63,20 @@ int main ( int argc, char** argv )
     vector<cv::KeyPoint> keypoints_1, keypoints_2;
     vector<cv::DMatch> matches;
     find_feature_matches ( img_1, img_2, keypoints_1, keypoints_2, matches );
-    cout<<"一共找到了"<<matches.size() <<"组匹配点"<<endl;
+    cout<<"Total of good matches: "<<matches.size() <<"\n";
 
-    //-- 估计两张图像间运动
+    //-- Estimate the motion between two images
     cv::Mat R,t;
  
     pose_estimation_2d2d ( keypoints_1, keypoints_2, matches, R, t );
    
 
-    //-- 三角化
+    //-- Triangulate
  
     vector<cv::Point3d> points;
     triangulation( keypoints_1, keypoints_2, matches, R, t, points );
  
-    //-- 验证三角化点与特征点的重投影关系
+    //-- Verify the reprojection relationship between triangulated points and feature points
     cv::Mat K = ( cv::Mat_<double> ( 3,3 ) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
     for ( int i=0; i<matches.size(); i++ )
     {
@@ -99,7 +89,7 @@ int main ( int argc, char** argv )
         cout<<"point in the first camera frame: "<<pt1_cam<<endl;
         cout<<"point projected from 3D "<<pt1_cam_3d<<", d="<<points[i].z<<endl;
         
-        // 第二个图
+        // Second picture
         cv::Point2f pt2_cam = pixel2cam( keypoints_2[ matches[i].trainIdx ].pt, K );
         cv::Mat pt2_trans = R*( cv::Mat_<double>(3,1) << points[i].x, points[i].y, points[i].z ) + t;
         pt2_trans /= pt2_trans.at<double>(2,0);
@@ -117,32 +107,33 @@ void find_feature_matches ( const cv::Mat& img_1, const cv::Mat& img_2,
                             std::vector<cv::KeyPoint>& keypoints_2,
                             std::vector< cv::DMatch >& matches )
 {
-    //-- 初始化
+    //-- initialization
     cv::Mat descriptors_1, descriptors_2;
-    // used in OpenCV3 
+    // used in OpenCV 
     cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
     cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
     // use this if you are in OpenCV2 
     // cv::Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create ( "cv::ORB" );
     // cv::Ptr<cv::DescriptorExtractor> descriptor = cv::DescriptorExtractor::create ( "cv::ORB" );
     cv::Ptr<cv::DescriptorMatcher> matcher  = cv::DescriptorMatcher::create("BruteForce-Hamming");
-    //-- 第一步:检测 Oriented FAST 角点位置
+    //-- Step 1: Detect the position of the Oriented FAST corner point
     detector->detect ( img_1,keypoints_1 );
     detector->detect ( img_2,keypoints_2 );
 
-    //-- 第二步:根据角点位置计算 BRIEF 描述子
+    //-- Step 2: Calculate the BRIEF descriptor based on the position of the corner point
     descriptor->compute ( img_1, keypoints_1, descriptors_1 );
     descriptor->compute ( img_2, keypoints_2, descriptors_2 );
 
-    //-- 第三步:对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
+    //-- Step 3: Match the BRIEF descriptors in the two images, using Hamming distance
     vector<cv::DMatch> match;
    // BFMatcher matcher ( NORM_HAMMING );
     matcher->match ( descriptors_1, descriptors_2, match );
 
-    //-- 第四步:匹配点对筛选
+    //-- The fourth step: matching point pair screening
     double min_dist=10000, max_dist=0;
 
-    //找出所有匹配之间的最小距离和最大距离, 即是最相似的和最不相似的两组点之间的距离
+    //Find the minimum and maximum distances between all matches, 
+    //that is, the distance between the most similar and least similar two sets of points
     for ( int i = 0; i < descriptors_1.rows; i++ )
     {
         double dist = match[i].distance;
@@ -153,7 +144,10 @@ void find_feature_matches ( const cv::Mat& img_1, const cv::Mat& img_2,
     printf ( "-- Max dist : %f \n", max_dist );
     printf ( "-- Min dist : %f \n", min_dist );
 
-    //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
+    //When the distance between the descriptors is greater than twice the minimum distance, 
+    //it is considered that the matching is wrong. 
+    //But sometimes the minimum distance will be very small, 
+    //and an empirical value of 30 is set as the lower limit.
     for ( int i = 0; i < descriptors_1.rows; i++ )
     {
         if ( match[i].distance <= max ( 2*min_dist, 30.0 ) )
@@ -169,10 +163,9 @@ void pose_estimation_2d2d (
     const std::vector< cv::DMatch >& matches,
     cv::Mat& R, cv::Mat& t )
 {
-    // 相机内参,TUM Freiburg2
+    // Camera internal reference, TUM Freiburg2
     cv::Mat K = ( cv::Mat_<double> ( 3,3 ) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1 );
-   cout <<"0\n";
-    //-- 把匹配点转换为vector<cv::Point2f>的形式
+    //-- Convert the matching point to the form of vector<cv::Point2f>
     vector<cv::Point2f> points1;
     vector<cv::Point2f> points2;
 
@@ -182,27 +175,25 @@ void pose_estimation_2d2d (
         points2.push_back ( keypoints_2[matches[i].trainIdx].pt );
     }
 
-    //-- 计算基础矩阵
-    cv::Mat fundamental_matrix;
-       cout <<"1\n";
+    //-- Calculate the fundamental matrix
+    cv::Mat fundamental_matrix;Calculate the fundamental matrix
     fundamental_matrix = cv::findFundamentalMat ( points1, points2, cv::FM_8POINT );
     cout<<"fundamental_matrix is "<<endl<< fundamental_matrix<<endl;
-     cout <<"2\n";
-    //-- 计算本质矩阵
-    cv::Point2d principal_point ( 325.1, 249.7 );				//相机主点, TUM dataset标定值
-    int focal_length = 521;						//相机焦距, TUM dataset标定值
+    //-- Calculate the essential matrix
+    cv::Point2d principal_point ( 325.1, 249.7 );				//Camera principal point, TUM dataset calibration value
+    int focal_length = 521;						//Camera focal length, TUM dataset calibration value
     cv::Mat essential_matrix;
     essential_matrix = cv::findEssentialMat ( points1, points2, focal_length, principal_point );
 
     cout<<"essential_matrix is "<<endl<< essential_matrix<<endl;
 
-    //-- 计算单应矩阵
+    //-- Calculate the homography matrix
     cv::Mat homography_matrix;
     homography_matrix = cv::findHomography ( points1, points2, cv::RANSAC, 3 );
     cout <<"3\n";
     cout<<"homography_matrix is "<<endl<<homography_matrix<<endl;
 
-    //-- 从本质矩阵中恢复旋转和平移信息.
+    //-- Recover rotation and translation information from the essential matrix.
     recoverPose ( essential_matrix, points1, points2, R, t, focal_length, principal_point );
     cout <<"4\n";
     cout<<"R is "<<endl<<R<<endl;
@@ -230,7 +221,7 @@ void triangulation (
     vector<cv::Point2f> pts_1, pts_2;
     for ( cv::DMatch m:matches )
     {
-        // 将像素坐标转换至相机坐标
+        // Convert pixel coordinates to camera coordinates
         pts_1.push_back ( pixel2cam( keypoint_1[m.queryIdx].pt, K) );
         pts_2.push_back ( pixel2cam( keypoint_2[m.trainIdx].pt, K) );
     }
@@ -238,7 +229,7 @@ void triangulation (
     cv::Mat pts_4d;
     triangulatePoints( T1, T2, pts_1, pts_2, pts_4d );
     
-    // 转换成非齐次坐标
+    // Convert to non-homogeneous coordinates
     for ( int i=0; i<pts_4d.cols; i++ )
     {
         cv::Mat x = pts_4d.col(i);
